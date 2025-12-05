3eacbcd53c48e49e6892c3d7be329fb1b7bdf828
Balajijakkala <jakkalabalaji789@gmail.com>
2025-12-05 11:21:32 +0530

    updated project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import "./Header.css";
 
+import p1 from "/src/assets/movies/m1.jpg";
+import p2 from "/src/assets/movies/m2.jpg";
+import p3 from "/src/assets/movies/m3.webp";
+import p4 from "/src/assets/movies/m4.webp";
+import p5 from "/src/assets/movies/m5.webp";
+import p6 from "/src/assets/movies/m6.webp";
+import p7 from "/src/assets/movies/m7.webp";
+import p8 from "/src/assets/movies/m8.webp";
+
 export default function Header({ theme }) {
-  const posters = [
-    "/src/assets/movies/m1.jpg",
-    "/src/assets/movies/m2.jpg",
-    "/src/assets/movies/m3.webp",
-    "/src/assets/movies/m4.webp",
-    "/src/assets/movies/m5.webp",
-    "/src/assets/movies/m6.webp",
-    "/src/assets/movies/m7.webp",
-    "/src/assets/movies/m8.webp",
-  ];
+  const posters = [p1, p2, p3, p4, p5, p6, p7, p8];
+
 
   return (
     <header className="header-container">
